03c51773f258e455d5f22156d46722985e35322a
Sebastian Manczak <sebmanczak@icloud.com>
2020-04-06 18:08:26 +0100

    update

--- src/main/java/com/kodilla/Blackjack.java
+++ src/main/java/com/kodilla/Blackjack.java
@@ -1,26 +1,47 @@
 package com.kodilla;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Blackjack extends Application {
+
+
+    private Image imageback = new Image("greentable.png");
+    private Image card = new Image("cards/2C.png");
+    private FlowPane cards = new FlowPane(Orientation.HORIZONTAL);
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 300, 300, Color.BLACK);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
 
-        Rectangle r = new Rectangle(25, 25, 250, 250);
-        r.setFill(Color.BLUE);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(10.5, 12.5, 13.5, 14.5));
+        grid.setHgap(5.5);
+        grid.setVgap(5.5);
+        grid.setBackground(background);
 
-        root.getChildren().add(r);
+        ImageView img = new ImageView(card);
+        cards.getChildren().add(img);
+
+        grid.add(cards, 0, 0, 3, 1);
+
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+
         primaryStage.setTitle("BlackJack");
         primaryStage.setScene(scene);
         primaryStage.show();
