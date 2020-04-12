d94949d8ccd62f829f2f5fc185055a29f88e2a3a
Sebastian Manczak <sebmanczak@icloud.com>
2020-04-12 21:15:07 +0100

    bjack

--- src/main/java/com/kodilla/Deck.java
+++ src/main/java/com/kodilla/Deck.java
@@ -1,15 +1,63 @@
 package com.kodilla;
 
-import javax.smartcardio.Card;
+import javafx.scene.image.Image;
+import java.util.*;
 
 public class Deck {
-    int number;
+    private java.util.List deck;
+    private int index;
 
-    public Deck(int number) {
-        this.number = number;
+    public Deck() {
+        this(1);
+    }
+    public Deck(int numDecks) {
+        deck = new ArrayList();
+        index = 0;
+
+        try {
+            for(int i = 0; i < numDecks; i++) {
+                Iterator suitIterator = Suit.VALUES.iterator();
+                while (suitIterator.hasNext()) {
+                    Suit suit = (Suit) suitIterator.next();
+                    Iterator rankIterator = Rank.VALUES.iterator();
+                    while(rankIterator.hasNext()) {
+                        Rank rank = (Rank) rankIterator.next();
+                        Card card = new Card(suit, rank, new Image(Card.getFilename(suit, rank)));
+                        addCard(card);
+                    }
+                }
+            }
+            shuffle();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
-    public int getNumber() {
-        return number;
+    public void addCard(Card card) {
+        deck.add(card);
+    }
+    public int getSizeOfDeck() {
+        return deck.size();
+    }
+    public int getNumberOfCardsRemaining() {
+        return deck.size() - index;
+    }
+    public Card dealCard() {
+        if (index >= deck.size())
+            return null;
+        else
+            return(Card) deck.get(index++);
+    }
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+    public boolean isEmpty() {
+        if (index >= deck.size())
+            return true;
+        else
+            return false;
+    }
+    public void restoreDeck() {
+        index = 0;
     }
 }
--- src/main/java/com/kodilla/Suit.java
+++ src/main/java/com/kodilla/Suit.java
@@ -1,4 +1,36 @@
 package com.kodilla;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Suit {
+    private String name;
+    private String symbol;
+
+    public final static Suit CLUBS = new Suit("Clubs", "c");
+    public final static Suit DIAMONDS = new Suit("Diamonds", "d");
+    public final static Suit HEARTS = new Suit("Hearts", "h");
+    public final static Suit SPADES = new Suit("Spades", "s");
+
+    public final static java.util.List VALUES =
+            Collections.unmodifiableList(
+                    Arrays.asList(new Suit[] { CLUBS, DIAMONDS, HEARTS, SPADES}));
+
+    public Suit(String nameValue, String symbolValue) {
+        this.name = nameValue;
+        this.symbol = symbolValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
